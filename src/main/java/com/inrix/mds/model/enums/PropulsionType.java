5d964f68f09e98e7a8ab66be8a8441d2484171f9
RyanBhandal <ryan.bhandal@inrix.com>
2024-06-05 16:51:37 +0100

    mds

---
+++ src/main/java/com/inrix/mds/model/enums/PropulsionType.java
@@ -0,0 +1,12 @@
+package com.inrix.mds.model.enums;
+
+public enum PropulsionType {
+    human,
+    electric_assist,
+    electric,
+    combustion,
+    combustion_diesel,
+    hybrid,
+    hydrogen_fuel_cell,
+    plug_in_hybrid
+}
---
+++ src/main/java/com/inrix/mds/model/enums/VehicleState.java
@@ -0,0 +1,13 @@
+package com.inrix.mds.model.enums;
+
+public enum VehicleState {
+    removed,
+    available,
+    non_operational,
+    reserved,
+    on_trip,
+    stopped,
+    non_contactable,
+    missing,
+    elsewhere
+}
