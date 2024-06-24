52b202ed5c19a221b9d139eddfdb187db9849c06
RyanBhandal <ryan.bhandal@inrix.com>
2024-06-24 16:29:19 +0100

    ENUM Values

--- src/main/java/com/inrix/mds/model/enums/PropulsionType.java
+++ src/main/java/com/inrix/mds/model/enums/PropulsionType.java
@@ -1,12 +1,18 @@
 package com.inrix.mds.model.enums;
 
 public enum PropulsionType {
-    human,
-    electric_assist,
-    electric,
-    combustion,
-    combustion_diesel,
-    hybrid,
-    hydrogen_fuel_cell,
-    plug_in_hybrid
+    human(1),
+    electric_assist(2),
+    electric(3),
+    combustion(4),
+    combustion_diesel(5),
+    hybrid(6),
+    hydrogen_fuel_cell(7),
+    plug_in_hybrid(8);
+
+    private final int val;
+
+    PropulsionType(int val) {
+        this.val = val;
+    }
 }
--- src/main/java/com/inrix/mds/model/enums/VehicleType.java
+++ src/main/java/com/inrix/mds/model/enums/VehicleType.java
@@ -1,15 +1,20 @@
 package com.inrix.mds.model.enums;
 
 public enum VehicleType {
-    bicycle,
-    bus,
-    cargo_bicycle,
-    car,
-    delivery_robot,
-    moped,
-    motorcycle,
-    scooter_standing,
-    scooter_seated,
-    truck,
-    other
+    bicycle(1),
+    bus(2),
+    cargo_bicycle(3),
+    car(4),
+    delivery_robot(5),
+    moped(6),
+    motorcycle(7),
+    scooter_standing(8),
+    scooter_seated(9),
+    truck(10),
+    other(11);
+    private final int val;
+
+    VehicleType(int val) {
+        this.val = val;
+    }
 }
